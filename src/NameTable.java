d78a40ee2e65ec3e7d14c75cedd2909f50328d8f
marika.jeziorna <marika.jeziorna@collibra.com>
2019-01-25 12:42:21 +0100

    zadanie-java-8 Added nameTable[3]

---
+++ src/NameTable.java
@@ -0,0 +1,10 @@
+public class NameTable {
+    public static void main(String[] args) {
+        String[] nameTables = new String[3];
+        nameTables[0] = "Marika";
+        nameTables[1] = "Anna";
+        nameTables[2] = "Karolina";
+
+        System.out.println("Show names list: " + nameTables[0] + ", " + nameTables[1] + ", " + nameTables[2]);
+    }
+}
